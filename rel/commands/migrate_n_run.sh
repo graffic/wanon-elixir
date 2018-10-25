f4723260800e5ac509f4a6f0b9b5c7ec764d82f2
Javier Gonel <bolibic@gmail.com>
2018-10-25 22:20:27 +0300

    fix(deploy): new migration script that stop services

--- rel/commands/migrate_n_run.sh
+++ rel/commands/migrate_n_run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
-$RELEASE_ROOT_DIR/bin/wanon command Elixir.Wanon.Migrations seed
+#$RELEASE_ROOT_DIR/bin/wanon command Elixir.Wanon.Migrations migrate
+release_ctl eval --mfa "Wanon.Migrations.migrate/1" --argv -- "$@"
 $RELEASE_ROOT_DIR/bin/wanon foreground
